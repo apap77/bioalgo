006fcf5432693128d24067c89c88cfe3a9057896
apap7 <apap7@naver.com>
2017-03-18 01:58:24 +0900

    adds travis ci

--- tests/seq.py
+++ tests/seq.py
@@ -1,5 +1,14 @@
 import sys; sys.path.append('../')
 import bioalgo.seq as seq
+import unittest
+
+class SeqTest(unittest.TestCase):
+
+	def test_dna_nucleotide_assertion(self):
+		self.assertRaises(AssertionError, seq.DNA, 'ATGCU')
+
+	def test_rna_nucleotide_assertion(self):
+		self.assertRaises(AssertionError, seq.RNA, 'ATGCU')
 
 if __name__ == '__main__':
 	d = seq.RNA('AUGAAUGAUUGA')
