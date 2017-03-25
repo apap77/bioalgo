9e60e56e3deab163434be060f80a32da79d3ea91
apap7 <apap7@naver.com>
2017-03-25 14:38:09 +0900

    implements DeBrujin.reconstruct

--- bioalgo/genome/assembly.py
+++ bioalgo/genome/assembly.py
@@ -1,19 +1,35 @@
 from collections import defaultdict
-
-def all_kmers_of(text, k):
-	for i in range(len(text) - k + 1):
-		yield text[i:i+k]
+from bioalgo.genome.util import all_kmers_of, eulerian_path
 
 class DeBrujin:
 
-	def __init__(self, text, k):
+	def __init__(self, text, k=3):
 		self.graph = defaultdict(list)
 
+		if type(text) is str:
+			self._construct_graph_from_text(text, k)
+		if type(text) is list :
+			self._construct_graph_from_patterns(text, k)
+
+		self.edges = []
+		for key, values in self.graph.items():
+			self.edges.extend([(key, value) for value in values])
+
+	def reconstruct_text(self):
+		eulerianPath = eulerian_path(self.edges)
+		return eulerianPath[0][0] + ''.join([src[-1] for src, dst in eulerianPath[1:]]) + eulerianPath[-1][1][-1]
+
+	def _construct_graph_from_text(self, text, k):
 		for kmer in all_kmers_of(text, k):
 			self.graph[kmer[:-1]].append(kmer[1:])
+
+	def _construct_graph_from_patterns(self, patterns, k):
+		for pattern in patterns:
+			for kmer in all_kmers_of(pattern, k):
+				self.graph[kmer[:-1]].append(kmer[1:])
 
 	def __getitem__(self, node):
 		return self.graph[node]
 
 	def __str__(self):
-		return '\n'.join(['%s: %s' % (k, ', '.join(v)) for k, v in sorted(self.graph.items())])
+		return '\n'.join(['%s -> %s' % (key, ', '.join(values)) for key, values in sorted(self.graph.items())])
--- tests/test_genome.py
+++ tests/test_genome.py
@@ -1,18 +1,26 @@
 import sys; sys.path.append('../')
 import bioalgo.genome as genome
 import unittest
-
 class AssemblyTest(unittest.TestCase):
 
 	def setUp(self):
-		self.db = genome.assembly.DeBrujin('TAATGCCATGGGATGTT', k=3)
+		patterns = ['GAGG', 'CAGG', 'GGGG', 'GGGA', 'CAGG', 'AGGG', 'GGAG']
+		self.dbText = genome.assembly.DeBrujin('TAATGCCATGGGATGTT', k=3)
+		self.dbText2 = genome.assembly.DeBrujin('TAATGCCATGGGATGTT', k=4)
+		self.dbPattern = genome.assembly.DeBrujin(patterns, k=4)
 
 	def test_de_brujin(self):
-		
-		self.assertEqual(self.db['TA'], ['AA'])
-		self.assertEqual(self.db['AT'], ['TG', 'TG', 'TG'])
-		self.assertEqual(self.db['TG'], ['GC', 'GG', 'GT'])
+		self.assertEqual(self.dbText['TA'], ['AA'])
+		self.assertEqual(self.dbText['AT'], ['TG', 'TG', 'TG'])
+		self.assertEqual(self.dbText['TG'], ['GC', 'GG', 'GT'])
+
+		self.assertEqual(self.dbPattern['AGG'], ['GGG'])
+		self.assertEqual(self.dbPattern['GGG'], ['GGG', 'GGA'])
 
 	def test_de_brujin_str(self):
-		s = 'AA: AT\nAT: TG, TG, TG\nCA: AT\nCC: CA\nGA: AT\nGC: CC\nGG: GG, GA\nGT: TT\nTA: AA\nTG: GC, GG, GT'
-		self.assertEqual(str(self.db), s)
+		s = 'AA -> AT\nAT -> TG, TG, TG\nCA -> AT\nCC -> CA\nGA -> AT\nGC -> CC\nGG -> GG, GA\nGT -> TT\nTA -> AA\nTG -> GC, GG, GT'
+		self.assertEqual(str(self.dbText), s)
+
+	def test_de_brujin_reconstruct_text(self):
+		self.assertEqual(self.dbText.reconstruct_text(), 'TAATGCCATGGGATGTT')
+		self.assertEqual(self.dbText2.reconstruct_text(), 'TAATGCCATGGGATGTT')
